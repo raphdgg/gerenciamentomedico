960e6283274a4ffa44a91694c379731e4fbdad21
Raphael <raphael.gregorio3@gmail.com>
2024-12-10 16:20:55 -0300

    feat: initial config

---
+++ src/main/java/com/gerenciamentomedico/GerenciamentomedicoApplication.java
@@ -0,0 +1,15 @@
+package com.gerenciamentomedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentomedicoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GerenciamentomedicoApplication.class, args);
+
+	}
+
+}
